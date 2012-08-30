c86acd87243950c943be8cb0e458d40f20c5042d
Dino <rock@onenix.net>
2012-08-30 17:59:20 +0700

    Update homepage right sitebar content.

--- themes/nava/blocks/block.right.php
+++ themes/nava/blocks/block.right.php
@@ -1,128 +1,32 @@
+<?php 
+
+$products = array();
+$filter = array();
+$filter['n.status'] = 1;
+
+$order = 'n.views DESC';
+
+$products = product::getProducts($filter, 0, 10, $order);
+
+?>
+
 <div class="show-case">
 	<h2 class="title-case">
 		<span>TOP 10 SẢN PHẨM</span>
 	</h2>
 	<ul>
-
+		<?php foreach($products as $k => $p) : ?>
 		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Móc khoá đậu cảm xúc - 29.000đ">
-				<span class="top">1</span>
-				<img src="./images/nava/banner/634753713757762500-540.jpg" alt="Móc khoá đậu cảm xúc" width="90" height="90" border="0" />
+			<a href="<?php echo $p->link_view ?>" class="img-top" title="<?php echo $p->title,' - ', $p->price ?>đ">
+				<span class="top"><?php echo $k+1 ?></span>
+				<img src="<?php echo $p->thumb ?>" alt="<?php echo $p->title ?>" width="90" height="90" border="0" />
 			</a>
 			<h4>
-				<a href="javascript:;" title="">Móc khoá đậu cảm xúc</a>
+				<a href="<?php echo $p->link_view ?>" title="<?php echo $p->title ?>"><?php echo $p->title ?></a>
 			</h4>
-			<p class="price">29.000đ</p>
+			<p class="price"><?php echo $p->price ?>đ</p>
 			<br class="clr" />
 		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Gối Handmade - Gối tình yêu - 195.000đ">
-				<span class="top">2</span>
-				<img src="./images/nava/banner/634617047278205000-854.jpg" alt="Gối Handmade - Gối tình yêu" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">Gối Handmade - Gối tình yêu</a>
-			</h4>
-			<p class="price">195.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="S22-sơ mi vai bồng tay lỡ - 180.000đ">
-				<span class="top">3</span>
-				<img src="./images/nava/banner/634807967703842500-563.jpg" alt="S22-sơ mi vai bồng tay lỡ" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">S22-sơ mi vai bồng tay lỡ</a>
-			</h4>
-			<p class="price">180.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Áo sơ mi nam phong cách MS 24 - 150.000đ">
-				<span class="top">4</span>
-				<img src="./images/nava/banner/634556751155361250-576.jpg" alt="Áo sơ mi nam phong cách MS 24" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">Áo sơ mi nam phong cách MS 24</a>
-			</h4>
-			<p class="price">150.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="HX230-Nước hoa hồng lancome - 250.000đ">
-				<span class="top">5</span>
-				<img src="./images/nava/banner/634807301971498750-372.jpg" alt="HX230-Nước hoa hồng lancome" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">HX230-Nước hoa hồng lancome</a>
-			</h4>
-			<p class="price">250.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Nước hoa Cool Water woman 100ml - 190.000đ">
-				<span class="top">6</span>
-				<img src="./images/nava/banner/634405303170951250-584.jpg" alt="Nước hoa Cool Water woman 100ml" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">Nước hoa Cool Water woman 100ml</a>
-			</h4>
-			<p class="price">190.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Cầu Pha lê phong thủy đúc tròn đều | Cho những gia đình theo xu hướng phong thủy - 270.000đ">
-				<span class="top">7</span>
-				<img src="./images/nava/banner/634658720592463750-897.jpg" alt="Cầu Pha lê phong thủy đúc tròn đều | Cho những gia đình theo xu hướng phong thủy" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">Cầu Pha lê phong thủy đúc tròn đều | Cho ...</a>
-			</h4>
-			<p class="price">270.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Quần jeans nhập khẩu - 359.000đ">
-				<span class="top">8</span>
-				<img src="./images/nava/banner/634606141683613750-473.jpg" alt="Quần jeans nhập khẩu" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">Quần jeans nhập khẩu</a>
-			</h4>
-			<p class="price">359.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Samsung galaxy Note N7000 - 5.400.000đ">
-				<span class="top">9</span>
-				<img src="./images/nava/banner/634793407503793750-604.jpg" alt="Samsung galaxy Note N7000" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">Samsung galaxy Note N7000</a>
-			</h4>
-			<p class="price">5.400.000đ</p>
-			<br class="clr" />
-		</li>
-
-		<li onmouseout="this.className=' ';" onmouseover="this.className='bgli';">
-			<a href="javascript:;" class="img-top" title="Dây chuyền ngắn chú chó lùn C201 - 60.000đ">
-				<span class="top">10</span>
-				<img src="./images/nava/banner/634693331360880416-793.jpg" alt="Dây chuyền ngắn chú chó lùn C201" width="90" height="90" border="0" />
-			</a>
-			<h4>
-				<a href="javascript:;" title="">Dây chuyền ngắn chú chó lùn C201</a>
-			</h4>
-			<p class="price">60.000đ</p>
-			<br class="clr" />
-		</li>
-
+		<?php endforeach ?>
 	</ul>
 </div>
